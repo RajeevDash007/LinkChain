2892d116df4a81e6cac96aee061bb92f28004d9b
Rajeev <rajeevdash07@gmail.com>
2024-05-30 17:35:54 +0530

    updated toggle button

--- src/components/forms/PageSettingsForm.js
+++ src/components/forms/PageSettingsForm.js
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from "react";
 
-export default function PageSettingsForm({page}) {
+export default function PageSettingsForm({ page }) {
   return (
-    <div className='-m-4'>
+    <div className="-m-4">
       <form>
-        <div className='bg-gray-300 h-32'>
+        <div className="bg-gray-300 h-32 flex justify-center items-center">
+          <div className="radio-togglers">
             <label>
-                <input type='radio' name='bgType' value="color"/>
-                <span className=''>Color</span>
+              <input type="radio" name="bgType" value="color" />
+              <span className="">Color</span>
             </label>
             <label>
-                <input type='radio' name='bgType' value="image"/>
-                <span className=''>Image</span>
+              <input type="radio" name="bgType" value="image" />
+              <span className="">Image</span>
             </label>
+          </div>
         </div>
         <div>avatar</div>
       </form>
     </div>
-  )
+  );
 }
